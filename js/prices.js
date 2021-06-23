f01a7ab6020115a812834d165e8d196e62c5c206
sisa95 <ponyesisa@gmail.com>
2021-06-23 17:14:24 +0200

    linked DOM with the logic

--- js/prices.js
+++ js/prices.js
@@ -1,1 +1,15 @@
+var nyangaTickets;
 
+if (localStorage['nyanga']) {
+    nyangaTickets = JSON.parse(localStorage.getItem("nyanga"))
+
+    var todayPrice = document.querySelector(".today_price")
+    var weeklyPrice = document.querySelector(".weekly_price")
+    var forthPrice = document.querySelector(".forth_price")
+    var monthlyPrice = document.querySelector(".monthly_price")
+
+    todayPrice.innerHTML = nyangaTickets.daily
+    weeklyPrice.innerHTML = nyangaTickets.weekly
+    forthPrice.innerHTML = nyangaTickets.two_weeks
+    monthlyPrice.innerHTML = nyangaTickets.monthly
+}
