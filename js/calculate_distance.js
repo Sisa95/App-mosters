1fe0ac0f7babe62d0c35baba73416488564ab468
sisa95 <ponyesisa@gmail.com>
2021-06-23 12:25:20 +0200

    ..

--- js/calculate_distance.js
+++ js/calculate_distance.js
@@ -1,4 +1,29 @@
-function func(x) {
+
+// function selectTo(destination) {
+//     alert(destination.value);
+//     switch (destination.value) {
+//         case "0":
+//             alert("selected 0");
+//             break;
+//         case "[-75.343, 39.984]":
+//             alert("Cape Town");
+//             break;
+//         case "2":
+//             alert("selected 2");
+//             break;
+//         case "3":
+//             alert("selected 3");
+//             break;
+//         case "4":
+//             alert("selected 4");
+//             break;
+//         default: // called if the value is none of the above
+//             alert("selected 5");
+//             break;
+//     }
+// }
+
+document.getElementById("selectFrom").onchange(function func(x) {
     alert(x.value);
     switch (x.value) {
         case "0":
@@ -20,29 +45,4 @@ function func(x) {
             alert("selected 5");
             break;
     }
-}
-function selectTo(destination) {
-    alert(destination.value);
-    switch (destination.value) {
-        case "0":
-            alert("selected 0");
-            break;
-        case "[-75.343, 39.984]":
-            alert("Cape Town");
-            break;
-        case "2":
-            alert("selected 2");
-            break;
-        case "3":
-            alert("selected 3");
-            break;
-        case "4":
-            alert("selected 4");
-            break;
-        default: // called if the value is none of the above
-            alert("selected 5");
-            break;
-    }
-}
-
-document.getElementById("selectFrom").addEventListener("onchange", func(x))
+})
--- js/prices.js
+++ js/prices.js
@@ -1,18 +1,1 @@
-// var features = turf.featureCollection([
-//     turf.point([-75.343, 39.984], {"name": "Location A"}),
-//     turf.point([-75.833, 39.284], {"name": "Location B"}),
-//     turf.point([-75.534, 39.123], {"name": "Location C"})
-//   ]);
-  
-//   var enveloped = turf.envelope(features);
-//GET TOWN from an to COORDINATES FROM THE LOCAL STORAGE
 
-
-var two1 = [5484544]
-
-
-var from = turf.point(two1);
-var to = turf.point([-75.534, 39.123]);
-var options = {units: 'miles'};
-
-var distance = turf.distance(from, to, options);
